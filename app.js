428f0016e14c1743c9d40007772bc53be9f01e30
TiagoSilva-dev <75397880+TiagoSilva-dev@users.noreply.github.com>
2021-03-04 13:27:34 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8877;
+
+app.get('/conversor',(req,res)=>{
+  res.json({
+    moedas:[
+      { key:0 , moeda:'BRL', name:'Brasil Real'},
+      { key:1 , moeda:'USD', name:'Estados Unidos Dólar'},
+      { key:2 , moeda:'EUR', name:'Zona do Euro'},
+      { key:3 , moeda:'GBP', name:'Libra Esterlina'},
+      { key:4 , moeda:'AUD', name: 'Austrália Dólar'},
+      { key:5 , moeda:'CHF', name: 'Franco Suíço'},
+      { key:6 , moeda:'CAD', name: 'Canadá Dólar '},
+      { key:7 , moeda:'ARS', name: 'Peso Argentino'},
+    ]
+  })
+})
+
+app.listen(PORT,()=>{})
