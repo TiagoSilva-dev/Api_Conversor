67d73504aa8646ae29c8d2fc9e9189c9364ac67a
TiagoSilva-dev <75397880+TiagoSilva-dev@users.noreply.github.com>
2021-03-04 14:35:47 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
-
 const express = require('express');
 const app = express();
 
-// Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || '0.0.0.0';
-// Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8877;
 
-var cors_proxy = require('cors-anywhere');
-cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, function() {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
-});
-
-app.get('/conversor',(req,res)=>{
+app.get('/conversor/',(req,res)=>{
   res.json({
     moedas:[
       { key:0 , moeda:'BRL', name:'Brasil Real'},
@@ -25,10 +12,11 @@ app.get('/conversor',(req,res)=>{
       { key:3 , moeda:'GBP', name:'Libra Esterlina'},
       { key:4 , moeda:'AUD', name: 'Austrália Dólar'},
       { key:5 , moeda:'CHF', name: 'Franco Suíço'},
-      { key:6 , moeda:'CAD', name: 'Canadá Dólar '},
+      { key:6 , moeda:'CAD' , name: 'Canadá Dólar '},
       { key:7 , moeda:'ARS', name: 'Peso Argentino'},
     ]
   })
 })
 
-app.listen(PORT,()=>{})
+app.listen(PORT,()=>{
+})
